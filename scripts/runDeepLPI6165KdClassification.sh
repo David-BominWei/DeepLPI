36360b66ac4df8b56f567b5e6c6b4b4d3e3113ff
david-wbm <weibomindd@163.com>
2022-10-20 20:38:07 +0800

    2022m10d19h9a1-lstm-revise-alpha

--- scripts/runDeepLPI6165KdClassification.sh
+++ scripts/runDeepLPI6165KdClassification.sh
@@ -1,7 +1,9 @@
 rm /home/wbm001/deeplpi/DeepLPI/output/tensorboard/*
 rm /home/wbm001/deeplpi/DeepLPI/output/model/*
 
-nohup python -u "/home/wbm001/deeplpi/DeepLPI/scripts/DeepLPI_6165_Kd_Classification.py" > "/home/wbm001/deeplpi/DeepLPI/output/nohup/network.out" 2>&1 &
+nohup python -u "/home/wbm001/deeplpi/DeepLPI/scripts/DeepLPI_6165_LSTM_Revise.py" > "/home/wbm001/deeplpi/DeepLPI/output/nohup/network.out" 2>&1 &
+
+killall tensorboard
 nohup tensorboard --logdir "/home/wbm001/deeplpi/DeepLPI/output/tensorboard" > "/home/wbm001/deeplpi/DeepLPI/output/nohup/tensorboard.out" 2>&1 &
 
 tail -fn 50 /home/wbm001/deeplpi/DeepLPI/output/nohup/network.out
